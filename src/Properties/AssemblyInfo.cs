72cfda86627e42aec93764cb8d952af15dced321
byvar <39142447+byvar@users.noreply.github.com>
2021-12-20 02:19:06 +0100

    Rename assembly temporarily

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("gaxm")]
+[assembly: AssemblyTitle("GaXM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("gaxm")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("GaXM")]
+[assembly: AssemblyCopyright("Created by Droolie, 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
